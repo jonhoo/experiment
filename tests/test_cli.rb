4f4e776a1a3ce711a02c80c1006ef16f7b800186
Jon Gjengset <jon@thesquareplanet.com>
2015-05-01 17:22:41 -0400

    Add --list to list versions and exit. Fixes #18

--- tests/test_cli.rb
+++ tests/test_cli.rb
@@ -5,7 +5,7 @@ require 'tmpdir'
 
 class TestCLI < ExperimentTestCase
 
-	def test_version
+	def test_single
 		@e["iterations"] = 10
 		@e["versions"] = {"a" => {}, "b" => {}, "c" => {}}
 		Dir.mktmpdir("test_", ".") {|d|
@@ -24,6 +24,25 @@ class TestCLI < ExperimentTestCase
 			@e.delete "repository"
 			experiment d, "--repository", File.absolute_path(d)
 			validate_run_dir(File.join(d, "out", "a", "run-1"))
+		}
+	end
+
+	def test_list
+		@e["versions"] = {"$letter" => {"vary" => {"letter" => "set(a, b, c)"}}}
+		Dir.mktmpdir("test_", ".") {|d|
+			build d
+			@e.delete "repository"
+			experiment d, "--list"
+			assert_false(File.exist? File.join(d, "out"))
+
+			vs = []
+			File.open File.join(d, "stdout.log"), 'r' do |f|
+				f.each_line do |l| vs.push l.strip end
+			end
+			assert_equal(3, vs.length)
+			assert_include(vs, 'a')
+			assert_include(vs, 'b')
+			assert_include(vs, 'c')
 		}
 	end
 
