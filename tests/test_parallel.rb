6699193ef3f927864000f5a572a245640b947201
Jon Gjengset <jon@thesquareplanet.com>
2015-05-01 17:42:57 -0400

    Up parallel timeout to avoid false neg in Travis

--- tests/test_parallel.rb
+++ tests/test_parallel.rb
@@ -7,7 +7,7 @@ class TestParallel < ExperimentTestCase
 
 	def test_iterations
 		@e["parallelism"] = 2
-		@e["arguments"] = ["$SRC/test", "3000"]
+		@e["arguments"] = ["$SRC/test", "5000"]
 		@e["versions"] = {"a" => {}, "b" => {}}
 		Dir.mktmpdir("test_", ".") {|d|
 			build d
@@ -16,7 +16,7 @@ class TestParallel < ExperimentTestCase
 			experiment d
 
 			dur = Time.now - start
-			assert_compare(dur, "<", 6)
+			assert_compare(dur, "<", 10)
 
 			a = File.stat File.join(d, "out", "a", "run-1", "experiment.log")
 			b = File.stat File.join(d, "out", "b", "run-1", "experiment.log")
