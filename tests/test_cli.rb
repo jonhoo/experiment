51f51cecfae3bfef6ff2fd026ddfa5c9c60f5c66
Jon Gjengset <jon@thesquareplanet.com>
2015-05-01 15:48:37 -0400

    Add -s for running a single version. Fixes #16

--- tests/test_cli.rb
+++ tests/test_cli.rb
@@ -5,6 +5,19 @@ require 'tmpdir'
 
 class TestCLI < ExperimentTestCase
 
+	def test_version
+		@e["iterations"] = 10
+		@e["versions"] = {"a" => {}, "b" => {}, "c" => {}}
+		Dir.mktmpdir("test_", ".") {|d|
+			build d
+			experiment d, "--single", "a"
+			validate_run_dir(File.join(d, "out", "a", "run-1"))
+			assert_false(File.exist? File.join(d, "out", "a", "run-2"))
+			assert_false(File.exist? File.join(d, "out", "b"))
+			assert_false(File.exist? File.join(d, "out", "c"))
+		}
+	end
+
 	def test_repository
 		Dir.mktmpdir("test_", ".") {|d|
 			build d
